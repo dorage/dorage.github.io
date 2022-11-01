d18f2b578e122341579d30c2c723f895c1f59fb8
HOME-DESKTOP <baloonflower554@gmail.com>
2022-11-01 22:51:12 +0900

    링크 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://dorage.github.io',
-    base: '/dorage.github.io',
+    base: '/',
     integrations: [mdx(), sitemap()],
 });
